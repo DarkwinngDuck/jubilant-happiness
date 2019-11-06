a1f541e60ad9ad2430a41268512abfb9b4475546
Maxim Grishkov <grishkov.m@gmail.com>
2019-11-06 23:27:50 +0200

    feat(domain): add ValueObject

---
+++ src/domain/value-object/ValueObject.spec.ts
@@ -0,0 +1,22 @@
+import { ValueObject } from './ValueObject';
+
+interface UserNameProperties {
+    name: string;
+}
+
+class UserName extends ValueObject<UserNameProperties> {}
+
+describe('[Domain] ValueObject', () => {
+    it('should be created', () => {
+        const userName = new UserName({ name: 'John' });
+        expect(userName).toBeDefined();
+    });
+
+    it('should equality of another value object', () => {
+        const john1 = new UserName({ name: 'John' });
+        const john2 = new UserName({ name: 'John' });
+        const bill = new UserName({ name: 'Bill' });
+        expect(john1.equals(john2)).toStrictEqual(true);
+        expect(bill.equals(john1)).toStrictEqual(false);
+    });
+});
